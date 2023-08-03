a0f634bec3be099e23ffebfb5ae51f83a9a9f635
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-08-03 19:55:44 -0300

    teste4 - userEdir

--- src/services/clientEdit.js
+++ src/services/clientEdit.js
@@ -1,6 +1,6 @@
 import { toastSucess } from "../helpers/ToastSuccess";
 import { toastError } from "../helpers/ToastError";
-import { api } from "../lib/axios"
+import api from "../lib/axios"
 
 export const clientEdit = async (clientData, id, token) => {
   try {
@@ -8,7 +8,7 @@ export const clientEdit = async (clientData, id, token) => {
     const response = await api.post(`/clients/edit/profile/${id}`, clientData);
 
     toastSucess("Dados do cliente atualizados com sucesso");
-    
+
     return response.data.client;
 
   } catch (error) {
