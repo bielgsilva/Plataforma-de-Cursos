9ccc32133e1142702e1f2e2a1fb312169a5024a6
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2023-08-03 19:46:32 -0300

    teste 2 - userEdit

--- src/services/clientEdit.js
+++ src/services/clientEdit.js
@@ -2,17 +2,13 @@ import { toastSucess } from "../helpers/ToastSuccess";
 import { toastError } from "../helpers/ToastError";
 import { api } from "../lib/axios"
 
-export const clientEdit = async (clientData, id, token, onClose) => {
+export const clientEdit = async (clientData, id, token) => {
   try {
 
-    const response = await api.post(`/clients/edit/profile/${id}`, clientData, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await api.post(`/clients/edit/profile/${id}`, clientData);
 
     toastSucess("Dados do cliente atualizados com sucesso");
-    onClose();
+    
     return response.data.client;
 
   } catch (error) {
